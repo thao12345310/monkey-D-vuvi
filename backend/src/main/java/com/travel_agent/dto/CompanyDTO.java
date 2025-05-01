fe2970c718f40f4a40fa6413c493c9fe84bd4c75
HoangManh <hoanmanh04@gmail.com>
2025-05-01 17:03:53 +0700

    Fix CRUD user and company

--- backend/src/main/java/com/travel_agent/dto/CompanyDTO.java
+++ backend/src/main/java/com/travel_agent/dto/CompanyDTO.java
@@ -2,15 +2,16 @@ package com.travel_agent.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Data
+@EqualsAndHashCode(callSuper = true)
 @JsonIgnoreProperties(ignoreUnknown = false)
-public class CompanyDTO {
+public class CompanyDTO extends AccountDTO{
     private Integer companyId;
-    private AccountDTO account;
     private String companyName;
 }
--- backend/src/main/java/com/travel_agent/models/entity/CompanyEntity.java
+++ backend/src/main/java/com/travel_agent/models/entity/CompanyEntity.java
@@ -6,27 +6,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "company")
-public class CompanyEntity {
+public class CompanyEntity extends AccountEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column (name = "company_id")
     private Integer companyId;
-
-    @OneToOne
-    @JoinColumn (name = "account_id")
-    private AccountEntity account;
 
     @Column(name = "company_name")
     private String companyName;
