644aae4ef0e9ccb2ae34f9f6f7256be53e100a57
HoangManh <hoanmanh04@gmail.com>
2025-05-08 04:03:23 +0700

    Docker compose

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,29 +10,29 @@ import ChiTietKhachSan from "./pages/ChiTietKhachSan";
 import "./App.css";
 import DoanhNghiep from "./pages/DoanhNghiep";
 import Footer from "./components/Footer";
-import DuThuyen from "./pages/DuThuyen"
+import DuThuyen from "./pages/DuThuyen";
 
 const App = () => {
-  return (
-    <Router>
-      <ScrollToTop />
-      <div className="min-h-screen bg-gray-50">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tim-du-thuyen" element={<TimDuThuyen />} />
-          <Route path="/tim-ve-may-bay" element={<TimVeMayBay />} />
-          <Route path="/tim-khach-san" element={<TimKhachSan />} />
-          <Route path="/khach-san/:id" element={<ChiTietKhachSan />} />
-          <Route path="/doanh-nghiep" element={<DoanhNghiep />} />
-          <Route path="/blog" element={<div>Blog</div>} />
-          <Route path="/lien-he" element={<div>Liên hệ</div>} />
-          <Route path="/du-thuyen" element={<DuThuyen />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <ScrollToTop />
+            <div className="min-h-screen bg-gray-50">
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/tim-du-thuyen" element={<TimDuThuyen />} />
+                    <Route path="/tim-ve-may-bay" element={<TimVeMayBay />} />
+                    <Route path="/tim-khach-san" element={<TimKhachSan />} />
+                    <Route path="/khach-san/:id" element={<ChiTietKhachSan />} />
+                    <Route path="/doanh-nghiep" element={<DoanhNghiep />} />
+                    <Route path="/blog" element={<div>Blog</div>} />
+                    <Route path="/lien-he" element={<div>Liên hệ</div>} />
+                    <Route path="/du-thuyen/:id" element={<DuThuyen />} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
 };
 
 export default App;
