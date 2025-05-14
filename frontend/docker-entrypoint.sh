87e75f02303efc85bf81fbe87d9cda6c73ef06f7
HoangManh <hoanmanh04@gmail.com>
2025-05-14 19:17:03 +0700

    fix nginx

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# Replace environment variables in nginx configuration
-envsubst '${VITE_BACKEND_HOST} ${VITE_BACKEND_PORT}  ${PORT} ${SERVER_NAME} ${CORS_ALLOWED_ORIGIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Replace environment variables in template
+envsubst '$PORT $SERVER_NAME $VITE_BACKEND_HOST $VITE_BACKEND_PORT $CORS_ALLOWED_ORIGIN' \
+  < /etc/nginx/templates/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
 
 # Start nginx
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
