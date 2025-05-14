166ad6cb4ef5be6c63b8a8352f6ff01b5572732f
HoangManh <hoanmanh04@gmail.com>
2025-05-14 15:07:11 +0700

    Dùng biến môi trường

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,19 +1,20 @@
+/// <reference types="vite/client" />
+
 // src/config.ts
 
 const config = {
     api: {
-      url: import.meta.env.VITE_API_URL || "http://localhost:8080",
+        url: import.meta.env.VITE_API_URL || "http://localhost:8080",
     },
     db: {
-      name: import.meta.env.VITE_DB_NAME || "monkey_d_vuvi",
-      username: import.meta.env.VITE_DB_USERNAME || "admin",
-      password: import.meta.env.VITE_DB_PASSWORD || "admin",
-      url: import.meta.env.VITE_DB_URL || "jdbc:postgresql://host.docker.internal:5432/monkey_d_vuvi",
+        name: import.meta.env.VITE_DB_NAME || "monkey_d_vuvi",
+        username: import.meta.env.VITE_DB_USERNAME || "admin",
+        password: import.meta.env.VITE_DB_PASSWORD || "admin",
+        url: import.meta.env.VITE_DB_URL || "jdbc:postgresql://host.docker.internal:5432/monkey_d_vuvi",
     },
     frontend: {
-      port: import.meta.env.VITE_FRONTEND_PORT || "5173",
+        port: import.meta.env.VITE_FRONTEND_PORT || "5173",
     },
-  };
-  
-  export default config;
-  
+};
+
+export default config;
