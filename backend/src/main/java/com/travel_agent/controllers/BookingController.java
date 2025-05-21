e0466ae403aab0f1912befefae36693127f90387
unknown <duongphuongthao08102004@gmail.com>
2025-05-21 09:59:49 +0700

    delete booking

--- backend/src/main/java/com/travel_agent/controllers/BookingController.java
+++ backend/src/main/java/com/travel_agent/controllers/BookingController.java
@@ -3,10 +3,7 @@ package com.travel_agent.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.travel_agent.dto.ResponseObject;
-import com.travel_agent.dto.hotel.BookingHotelRoomDTO;
-import com.travel_agent.services.BookingHotelRoomService;
-import com.travel_agent.services.BookingShipRoomService;
-import com.travel_agent.dto.BookingShipRoomDTO;
+
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,18 +12,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BookingController {
 
-    private final BookingHotelRoomService bookingHotelRoomService;
-    private final BookingShipRoomService bookingShipRoomService;
+    // private final BookingHotelRoomService bookingHotelRoomService;
+    // private final BookingShipRoomService bookingShipRoomService;
 
-    @PostMapping("/hotel")
-    public ResponseEntity<ResponseObject> createHotelBooking(@RequestBody BookingHotelRoomDTO bookingDTO) {
-        return ResponseEntity.ok(bookingHotelRoomService.createBooking(bookingDTO));
-    }
+    // @PostMapping("/hotel")
+    // public ResponseEntity<ResponseObject> createHotelBooking(@RequestBody BookingHotelInfoDTO bookingDTO) {
+    //     return ResponseEntity.ok(bookingHotelRoomService.createBooking(bookingDTO));
+    // }
 
-    @PostMapping("/ship")
-    public ResponseEntity<ResponseObject> createShipBooking(@RequestBody BookingShipRoomDTO bookingDTO) {
-        return ResponseEntity.ok(bookingShipRoomService.createBooking(bookingDTO));
-    }
+    // @PostMapping("/ship")
+    // public ResponseEntity<ResponseObject> createShipBooking(@RequestBody BookingShipInfoDTO bookingDTO) {
+    //     return ResponseEntity.ok(bookingShipRoomService.createBooking(bookingDTO));
+    // }
 
     // @GetMapping
     // public ResponseEntity<ResponseObject> getBookingHotelRoomByHotelId() {
--- backend/src/main/java/com/travel_agent/repositories/hotel/BookingHotelInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.travel_agent.repositories.hotel;
-
-import com.travel_agent.models.entity.hotel.BookingHotelInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingHotelInfoRepository extends JpaRepository<BookingHotelInfo, Long> {
-} 
